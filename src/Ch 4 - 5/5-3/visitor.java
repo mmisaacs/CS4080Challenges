34c7f45a843e536e5293466cd4ccfb285e951ca0
mmisaacs <miss.maddie.isaacs@gmail.com>
2026-02-12 22:03:36 -0800

    visitor class for 5-3

---
+++ src/Ch 4 - 5/5-3/visitor.java
@@ -0,0 +1,29 @@
+class RpnPrinter implements Expr.Visitor<String> {
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " +
+                expr.right.accept(this) + " " +
+                expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+}
